98f7449d9e9448aaded245e367b41d9b6748a062
glenmason <gsm@mailbox.org>
2024-07-08 08:48:17 +1000

    Add files via upload

--- start/Start.gd
+++ start/Start.gd
@@ -25,29 +25,29 @@ func _on_combat_button_down():
 
 func load_game():
 
-	Data.get_node("PC1").dict = Data.load_game("user://pc1_save.save")
+	Data.get_node("PC1").dict = Data.load_game("user://pc1.save")
 	if Data.get_node("PC1").dict["first_name"] == null:
 		Main.pc_button_disable(1)
 		Main.pc_button_hide("HBox/Party/PC1Frame/PC1")
 		
-	Data.get_node("PC2").dict = Data.load_game("user://pc2_save.save")
+	Data.get_node("PC2").dict = Data.load_game("user://pc2.save")
 	if Data.get_node("PC2").dict["first_name"] == null:
 		Main.pc_button_disable(2)
 		Main.pc_button_hide("HBox/Party/PC2Frame/PC2")
 	
-	Data.get_node("PC3").dict = Data.load_game("user://pc3_save.save")
+	Data.get_node("PC3").dict = Data.load_game("user://pc3.save")
 	if Data.get_node("PC3").dict["first_name"] == null:
 		Main.pc_button_disable(3)
 		Main.pc_button_hide("HBox/Party/PC3Frame/PC3")
 	
-	Data.get_node("PC4").dict = Data.load_game("user://pc4_save.save")
+	Data.get_node("PC4").dict = Data.load_game("user://pc4.save")
 	if Data.get_node("PC4").dict["first_name"] == null:
 		Main.pc_button_disable(4)
 		Main.pc_button_hide("HBox/Party/PC4Frame/PC4")
 	
-	Data.inventory = Data.load_game("user://inventory_save.save")
-	Data.stats = Data.load_game("user://stats_save.save")
-	Data.time_dict = Data.load_game("user://time_save.save")
+	Data.inventory = Data.load_game("user://inventory.save")
+	Data.stats = Data.load_game("user://stats.save")
+	Data.time_dict = Data.load_game("user://time.save")
 	Data.time = Data.time_dict["hour"]
 	
 func new_game():
@@ -60,3 +60,4 @@ func new_game():
 	Data.new_player("PC4")
 	
 	Data.save_game()
+	Data.save_notes("")
